6bd7d6db294053b173bc07a9636237a3078a4c02
martin <martin@aflotemx.com>
2023-10-21 19:23:46 -0600

    add docker files and web icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,43 @@
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-config.autoAddCss = false
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 
-import './globals.css'
-import type { Metadata } from 'next'
+import "./globals.css";
+import type { Metadata } from "next";
 export const metadata: Metadata = {
-  title: 'Web',
-  description: 'Next.js + Tailwind + FontAwesome Web',
-}
+  title: "Web",
+  description: "Nextjs web with Tailwind and FontAwesome.",
+  icons: [
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      url: "/apple-touch-icon.png",
+      href: "/apple-touch-icon.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      url: "/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      url: "/favicon-16x16.png",
+    },
+    { rel: "manifest", url: "/site.webmanifest" },
+  ],
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import { faFontAwesome } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import Image from 'next/image'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFontAwesome } from "@fortawesome/free-regular-svg-icons";
 
 export default function Home() {
   return (
     <main>
-    <FontAwesomeIcon icon={faFontAwesome} className='text-9xl bg-green-400' />
+      <FontAwesomeIcon icon={faFontAwesome} className="text-9xl bg-green-400" />
     </main>
-      )
+  );
 }
